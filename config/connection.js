6faefc2f66ad2f0457ead466ab9a4924b4e26171
JackYouk <jackyoukstetter1@gmail.com>
2022-10-03 20:03:06 -0700

    added code for connection and server, installed packages, added blank files for file structure

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+require('dotenv');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
